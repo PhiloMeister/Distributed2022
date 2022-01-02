fbc2658bedfbd7253ca10480fa25e5a409eaf92c
Philomeister <abdullah.binjos@students.hevs.ch>
2022-01-02 18:55:25 +0100

    Abdullah binjos

--- src/main/java/Client/ListOfClients.java
+++ src/main/java/Client/ListOfClients.java
@@ -4,35 +4,36 @@ import Model.ClientModel;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ListOfClients {
 
-    HashMap<Integer,ClientModel> listOfClient = new HashMap<Integer, ClientModel>();
+
+   private CopyOnWriteArrayList <ClientModel> listOfCLients = new CopyOnWriteArrayList<ClientModel>();
 
     public ListOfClients() throws IOException {
         ClientModel client1 = new ClientModel("PhiloMeister","IsYourMomFree","0.0.0.0",1234);
         ClientModel client2 = new ClientModel("Nicolas","Nicolas","0.0.0.0",1234);
         ClientModel client3 = new ClientModel("JesseLivermore","JesseLivermore","0.0.0.0",1234);
         ClientModel client4 = new ClientModel("MorganHousel","MorganHousel","0.0.0.0",1234);
-        listOfClient.put(1,client1);
-        listOfClient.put(2,client2);
-        listOfClient.put(3,client3);
-        listOfClient.put(4,client4);
+        listOfCLients.add(client1);
+        listOfCLients.add(client2);
+        listOfCLients.add(client3);
+        listOfCLients.add(client4);
 
-        }
-
-    public HashMap<Integer, ClientModel> getListOfClient() {
-        return listOfClient;
     }
 
-    public void setListOfClient(HashMap<Integer, ClientModel> listOfClient) {
-        this.listOfClient = listOfClient;
+    public CopyOnWriteArrayList<ClientModel> getListOfCLients() {
+        return listOfCLients;
     }
 
-    public void addToListOfClient(ClientModel clientModel) {
-
-
-        }
+    public void setListOfCLients(CopyOnWriteArrayList<ClientModel> listOfCLients) {
+        this.listOfCLients = listOfCLients;
     }
+    public ClientModel getClient(int index) {
+        return listOfCLients.get(index);
+    }
+
+}
     
 
--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -1,28 +1,27 @@
 package Server;
 
-import Client.Client;
+
+import Client.ListOfClients;
 import Model.ClientModel;
 
 import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.List;
+
 import java.util.Scanner;
 
 public class Server {
+    ListOfClients listOfClients = new ListOfClients();
+    BufferedReader bufferedReader;
+    BufferedWriter bufferedWriter;
+    Scanner scannerIn = new Scanner(System.in);
     private Socket srvSocket;
     private InetAddress localAddress = null;
     private ServerSocket mySkServer;
-
     private ClientModel clientModel;
-    List<ClientModel> listOfClientModel;
-
     private String ipadress;
     private int portUsed;
-    BufferedReader bufferedReader;
-    BufferedWriter bufferedWriter;
-    Scanner scanner;
 
     public Server(String ipadress, int portUsed) throws IOException, ClassNotFoundException, InterruptedException {
         this.ipadress = ipadress;
@@ -30,38 +29,50 @@ public class Server {
 
         //CREATION OF SERVER
         //AND WAITING FOR A CLIENT..
-        createServer(ipadress,portUsed,mySkServer,srvSocket);
-        loginCheck(srvSocket);
+        createServer();
+        loginCheck();
 
 
     }
 
-    private void loginCheck(Socket srvSocket) throws IOException, ClassNotFoundException {
+    private void loginCheck() throws IOException, ClassNotFoundException {
         // get the input stream from the connected socket
+        boolean isAuthentified = false;
         String username;
         String password;
 
         srvSocket = mySkServer.accept();
+        System.out.println("Client connected");
 
         BufferedReader buffin = new BufferedReader(new InputStreamReader(srvSocket.getInputStream()));
         PrintWriter pout = new PrintWriter(srvSocket.getOutputStream(), true);
 
-        while(isAuthentified()==false){
+        while (isAuthentified != true) {
             pout.println("//USERNAME//");
-            username = scanner.nextLine();
+            username = scannerIn.nextLine();
             pout.println("//PASSWORD//");
-            password = scanner.nextLine();
-            ClientModel clientTest = new ClientModel(username,password);
+            password = scannerIn.nextLine();
+            ClientModel clientTest = new ClientModel(username, password);
+
+
+            for (ClientModel client : listOfClients.getListOfCLients()) {
+                if (client.getUsername() == clientTest.getUsername() && client.getPassword() == clientTest.getPassword()) {
+                    isAuthentified = true;
+                } else {
+                    System.out.println("!WRONG PASSWORD OR LOGIN!");
+                }
+            }
+
         }
         // create a DataInputStream so we can read data from it.
         System.out.println("[SERVER] : clients info received");
         System.out.println("Client infos :");
-        System.out.println("username :"+ clientModel.getUsername());
+        System.out.println("username :" + clientModel.getUsername());
         System.out.println();
 
     }
 
-    private void createServer(String ipadress, int portUsed,ServerSocket mySkServer,Socket srvSocket) throws IOException, InterruptedException {
+    private void createServer() throws IOException, InterruptedException {
         mySkServer = new ServerSocket(portUsed, 10, InetAddress.getByName(ipadress));
         System.out.println("//-VSFY- ONLINE//");
         System.out.println("Default Timeout : " + mySkServer.getSoTimeout());
@@ -70,10 +81,7 @@ public class Server {
         System.out.println("waiting..");
 
 
-
-
     }
-
 
 
 }
